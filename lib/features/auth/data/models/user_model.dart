bfbd114ba4c3f87edd4f2e04603f9e78f66b8d7a
DonaSoft <donasoft.team@gmail.com>
2023-03-21 12:26:54 -0400

    User & userModel

--- lib/features/auth/data/models/user_model.dart
+++ lib/features/auth/data/models/user_model.dart
@@ -1,5 +1,55 @@
+import 'package:pentelligence/core/constant/constant.dart';
 import 'package:pentelligence/features/auth/models/entities/user.dart';
 
-class UserModel extends User{
-  
+class UserModel extends User {
+  UserModel(
+      int id,
+      String name,
+      String email,
+      String phoneNumber,
+      String token,
+      String imageUrl,
+      String isVerified,
+      UserType userType,
+      List<Object> Courses,
+      List<Object> Channel)
+      : super(
+            id: id,
+            name: name,
+            email: email,
+            phoneNumber: phoneNumber,
+            token: token,
+            imageUrl: imageUrl,
+            isVerified: isVerified,
+            userType:userType,
+            Courses: Courses,
+            Channel: Channel);
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+        json['id'],
+        json['name'],
+        json['email'],
+        json['phoneNumber'],
+        json['token'],
+        json['imageUrl'],
+        json['isVerified'],
+        json['Courses'],
+        json['userType'],
+        json['Channel']);
+  }
+
+  Map<String, dynamic> toMap(User user) {
+    return {
+      "id": user.id,
+      "name": user.name,
+      "email": user.email,
+      "phoneNumber": user.phoneNumber,
+      "token": user.token,
+      "imageUrl": user.imageUrl,
+      "isVerified": user.isVerified,
+      "Courses": user.Courses,
+      "Channel": user.Channel
+    };
+  }
 }
--- lib/features/auth/models/entities/user.dart
+++ lib/features/auth/models/entities/user.dart
@@ -1,8 +1,39 @@
 import 'package:equatable/equatable.dart';
+import 'package:pentelligence/core/constant/constant.dart';
 
-class User extends Equatable{
+class User extends Equatable {
+  User(
+      {required this.id,
+      required this.name,
+      required this.email,
+      required this.phoneNumber,
+      required this.token,
+      required this.imageUrl,
+      required this.isVerified,
+      required this.userType,
+      required this.Courses,
+      required this.Channel});
+
   @override
   // TODO: implement props
-  List<Object?> get props => throw UnimplementedError();
+  List<Object?> get props => [id, name, email, token];
 
+  final int id;
+  final String name;
+  final String email;
+  final String phoneNumber;
+  final String token;
+  final String imageUrl;
+  final String isVerified;
+  final UserType userType;
+  List<Object> Courses;
+  List<Object> Channel;
+
+  List<Object> get getList {
+    return Channel;
+  }
+
+  set setList(List<Object> value) {
+    Channel = value;
+  }
 }
